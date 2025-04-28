35a5dfff2c0a0d4fd5c0bff39872e260acd9a8d5
karoene <karoene.d@hotmail.com>
2025-04-28 12:23:44 -0300

    iniciando

---
+++ README.md
@@ -0,0 +1,7 @@
+# API PARA GERENCIAMENTO DE CATÁLOGO DE FILMES
+
+Desenvolvida com JAVA, API RESTfull, Webservice, conceitos de POO, lógica, etc.
+
+
+
+Autora: Karoene Mendonça
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        String filme1 = tec.nextLine();
+
+        System.out.println(filme1);
+    }
+
+
+
+}
